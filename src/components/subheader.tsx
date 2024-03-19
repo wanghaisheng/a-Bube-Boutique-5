88a77e27bf0ec5ab5b9683c49043ea6fb4f5d5a4
immanuellking <emmaebubeegbosi@gmail.com>
2024-03-19 17:40:18 +0100

    made sub header a marque added necessary extensions totailind configs style also added fonts

---
+++ src/components/subheader.tsx
@@ -0,0 +1,10 @@
+export default function SubHeader() {
+  return (
+    <div className="w-full flex bg-black flex-nowrap">
+      <span className="flex items-center justify-center text-white text-[13px] font-custom py-2 font-semibold tracking-wider md:justify-start animate-infinite-scroll">
+        $10 EXPRESS COURIER. FREE SHIPPING FOR ORDERS $200+. SUPRISE GIFTS WITH
+        PURCHASES $250+.
+      </span>
+    </div>
+  );
+}

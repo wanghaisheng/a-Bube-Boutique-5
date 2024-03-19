9e8323b2fb1b6db12108899394332f018c11f284
immanuellking <emmaebubeegbosi@gmail.com>
2024-03-19 17:39:34 +0100

    added home page component to the root page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import HomePage from "@/pages/homepage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <HomePage />
+    </main>
+  );
+}

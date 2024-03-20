79d036b86fa39abbdb3369dbd43941d01ebbf52e
immanuellking <emmaebubeegbosi@gmail.com>
2024-03-20 20:49:59 +0100

    added custom font root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="overflow-x-hidden">
+      <body className="overflow-x-hidden font-custom">
         <Header />
         {children}</body>
     </html>

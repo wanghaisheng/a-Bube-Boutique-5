18c6b21d3528aa7f14d52cf009ddd0ecb71bfcfe
immanuellking <emmaebubeegbosi@gmail.com>
2024-03-21 14:08:48 +0100

    added style to make text not break on smaller screens

--- src/components/subheader.tsx
+++ src/components/subheader.tsx
@@ -1,7 +1,7 @@
 export default function SubHeader() {
   return (
     <div className="w-full flex bg-black flex-nowrap">
-      <span className="flex items-center justify-center text-white text-[13px] font-custom py-2 font-semibold tracking-wider md:justify-start animate-infinite-scroll">
+      <span className="flex items-center justify-center text-white text-xs sm:text-[13px] text-nowrap font-custom py-2 font-semibold tracking-wider md:justify-start animate-infinite-scroll">
         $10 EXPRESS COURIER. FREE SHIPPING FOR ORDERS $200+. SUPRISE GIFTS WITH
         PURCHASES $250+.
       </span>

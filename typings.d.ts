e6ffb1f166eea9a943fcb6500cd6be1c20418097
immanuellking <emmaebubeegbosi@gmail.com>
2024-03-30 06:48:19 +0100

    created types file for sanity data

---
+++ typings.d.ts
@@ -0,0 +1,26 @@
+import { Image } from "sanity";
+
+type SanityBase = {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _updatedAt: string;
+};
+
+// interface Image {
+//     // Define the properties of your image object
+//     url: () => any; // Assuming the URL is a string
+//     // Add other properties if necessary
+//   }
+
+ interface NewInItems extends SanityBase {
+  _type: "newIn";
+  name: string;
+  slug: string;
+  cardText: string;
+  price: number;
+  colors: string[];
+  description: string;
+  images: Image[];
+  imageTest: Image;
+}

ef3739ef0a815e77872546222449fd73f11bcfb8
immanuellking <emmaebubeegbosi@gmail.com>
2024-03-30 06:53:36 +0100

    done with hero section, now responsive and slider working

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Mousewheel, Autoplay } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules"; //Pagination, Navigation, Mousewheel
 
 // Import Swiper styles
 import "swiper/css";
@@ -34,28 +34,28 @@ const images: Images[] = [
 
 export default function Hero() {
   return (
-    <section className="w-full h-[75vh]">
+    <section className="w-full h-[60vh] sm:h-[55vh] lg:h-[75vh]">
       <Swiper
         className="mySwiper bg-red-500 h-full w-full"
-        // direction={'vertical'}
+        direction={"horizontal"}
         slidesPerView={1}
         spaceBetween={0}
         loop={true}
         mousewheel={true}
         autoplay={{
-          delay: 4000,
+          delay: 6000,
           disableOnInteraction: false,
         }}
         pagination={{
           clickable: true,
         }}
-        navigation={true}
-        modules={[Pagination, Navigation, Mousewheel, Autoplay]}
+        // navigation={true}
+        modules={[Autoplay, Pagination]} //Pagination, Navigation,Mousewheel
       >
-        {images.map((item) => {
+        {images.map((item, index) => {
           const { src, text } = item;
           return (
-            <SwiperSlide className="w-full h-full relative">
+            <SwiperSlide className="w-full h-full relative" key={index}>
               <Image
                 src={src}
                 alt={text}
@@ -63,10 +63,12 @@ export default function Hero() {
                 className="w-full h-full object-cover"
               />
               <div className="absolute bg-black opacity-25 top-0 right-0 left-0 bottom-0 z-20 inset-0"></div>
-              <div className="z-50 absolute bottom-[20%] left-[50%] -translate-x-[50%]">
+              <div className="z-50 absolute bottom-[10%] lg:bottom-[20%] left-[50%] -translate-x-[50%]">
                 <div className="flex flex-col items-center gap-6">
-                  <p className="text-3xl text-white">BUBE {text}</p>
-                  <button className="border-white border-[1.5px] py-3 px-8 text-white transition-all duration-500 hover:ring-2 hover:ring-white ">
+                  <p className="text-2xl lg:text-3xl text-white text-nowrap">
+                    BUBE {text}
+                  </p>
+                  <button className="border-white border-[1.5px] text-nowrap py-2 lg:py-3 px-8 text-white transition-all duration-500 hover:ring-2 hover:ring-white ">
                     BUBE {text}
                   </button>
                 </div>

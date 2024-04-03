1e3f7d895d31e44789b753299262b9b61ddc787f
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-03 12:53:37 +0100

    added flex and overscroll for mobile, grid view for web, new in section now responsive

--- src/components/newIn/newIn.tsx
+++ src/components/newIn/newIn.tsx
@@ -8,9 +8,9 @@ export default async function NewIn() {
   // console.log(newInItems)
   return (
     <section className="py-12">
-      <div className="w-[70%] mx-auto space-y-8">
-        <h1 className="text-4xl">BUBE NEW IN & MUST HAVE!</h1>
-        <div className="grid grid-cols-12 gap-4">
+      <div className="w-[90%] lg:w-[70%] mx-auto space-y-8">
+        <h1 className="text-3xl sm:text-4xl">BUBE NEW IN & MUST HAVE!</h1>
+        <div className="flex snap-x snap-mandatory sm:grid sm:grid-cols-12 gap-4 overflow-auto">
           {newInItems.map((item, index) => {
             return <ProductCard newInItems={item} key={item._id} />;
           })}

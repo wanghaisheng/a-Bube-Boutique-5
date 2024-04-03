fa0e28c88e6c420c97da01670fe960956fb6d9a8
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-03 12:52:59 +0100

    product card now responsive for all screens

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -16,8 +16,8 @@ export default function ProductCard({
   const { name, images, cardText, slug } = newInItems;
 
   return (
-    <div className="basis-[50%] col-span-4 bg-gray-100">
-      <div className="relative w-full h-[22rem]">
+    <div className="w-[25rem] sm:w-full sm:basis-[50%] sm:col-span-6 bg-gray-100 snap-center snap-always">
+      <div className="relative w-[25rem] sm:w-full h-[22rem] sm:h-[25rem] lg:h-[22rem]">
         <Image src={urlForImage(images[0])} alt={name} className="" fill />
       </div>
       <div className="text-center py-4 px-4 space-y-2">

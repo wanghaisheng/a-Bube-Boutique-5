f85840fb4295804e83d9056903c54833bc86fbf9
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-05 10:50:30 +0100

    updated import for product card component

--- src/components/newIn/newIn.tsx
+++ src/components/newIn/newIn.tsx
@@ -1,4 +1,4 @@
-import ProductCard from "../productCard";
+import ProductCard from "../product/productCard";
 import { getNewInProducts } from "@/queries/getProducts";
 
 // export const revalidate = 60;

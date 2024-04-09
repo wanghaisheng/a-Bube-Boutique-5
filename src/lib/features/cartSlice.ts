7fc01fc3d7a7c8d09af90a2524e86e2a0738fdf1
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-09 17:40:35 +0100

    cart slice, typed initial state, exported action and reducer

---
+++ src/lib/features/cartSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { CartType } from "../../../typings";
+import { PayloadAction } from "@reduxjs/toolkit";
+
+interface InitialState {
+  cart: CartType[];
+}
+
+const initialState: InitialState = {
+  cart: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action: PayloadAction<CartType>) {
+      console.log("Working");
+      const cartItemExist = state.cart.find((item) =>
+        item.id === action.payload.id ? item : null
+      );
+      if (cartItemExist) {
+        const updatedCartItem = state.cart.map((item) =>
+          item.id === cartItemExist.id
+            ? {
+                ...item,
+                quantity: item.quantity + action.payload.quantity,
+                total: item.total + action.payload.total,
+              }
+            : item
+        );
+        state.cart = updatedCartItem;
+        console.log("Multiple", state.cart);
+      } else {
+        state.cart = [...state.cart, action.payload];
+        console.log("Single", state.cart);
+      }
+      localStorage.setItem("cartLocal", JSON.stringify(state.cart));
+    },
+    getCartFromLocalStorage(state) {
+      const cartLocal = localStorage.getItem("cartLocal");
+      if (cartLocal !== null) {
+        state.cart = JSON.parse(cartLocal);
+      } else {
+        state.cart = [];
+      }
+    },
+  },
+});
+
+export const { addToCart, getCartFromLocalStorage } = cartSlice.actions;
+export default cartSlice.reducer;

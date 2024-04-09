9956b0300acd5672eb2053d1465a63bf4843a403
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-09 17:42:50 +0100

    handled error or empty items

--- src/components/newIn/newIn.tsx
+++ src/components/newIn/newIn.tsx
@@ -6,15 +6,24 @@ import { getNewInProducts } from "@/queries/getProducts";
 export default async function NewIn() {
   const newInItems = await getNewInProducts();
   // console.log(newInItems)
+
   return (
     <section className="py-12">
       <div className="w-[90%] lg:w-[70%] mx-auto space-y-8">
         <h1 className="text-3xl sm:text-4xl">BUBE NEW IN & MUST HAVE!</h1>
-        <div className="flex snap-x snap-mandatory sm:grid sm:grid-cols-12 gap-4 overflow-auto">
-          {newInItems.map((item, index) => {
-            return <ProductCard newInItems={item} key={item._id} />;
-          })}
-        </div>
+        {newInItems.length === 0 ? (
+          <div className="w-full h-[40vh] flex items-center justify-center">
+            <h4 className="italic text-2xl">
+              Couldn't retrieve New In Items 😔
+            </h4>
+          </div>
+        ) : (
+          <div className="flex snap-x snap-mandatory sm:grid sm:grid-cols-12 gap-4 overflow-auto">
+            {newInItems.map((item, index) => {
+              return <ProductCard newInItems={item} key={item._id} />;
+            })}
+          </div>
+        )}
       </div>
     </section>
   );

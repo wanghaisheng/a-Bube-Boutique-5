3a18c669347ca4ad8e802e1bd91d637fe9e97750
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-13 13:22:16 +0100

    changed layout from table elements to divs and adjusted the grid cols

--- src/app/(client)/cart/page.tsx
+++ src/app/(client)/cart/page.tsx
@@ -1,3 +1,5 @@
+import CartItems from "@/components/cart/cartItems";
+
 export default function CartPage() {
   return (
     <div className="">
@@ -6,27 +8,23 @@ export default function CartPage() {
           <h1 className="text-5xl">Your Cart</h1>
           <p className="underline">Continue Shopping</p>
         </div>
-        <table className="w-full">
-          <thead className="w-full">
-            <tr className="grid grid-cols-12 border-b-[1px] border-gray-400 pb-4">
-              <th className="text-left col-span-2">
-                <p className="font-light text-gray-500">product</p>
-              </th>
-              <th className="text-left bg-red-200 col-span-5">
-                <p className="opacity-0">description</p>
-              </th>
-              <th className="text-left bg-blue-300 col-span-3">
-                <p className="font-light text-gray-500">quantity</p>
-              </th>
-              <th className="text-left  bg-green-200 col-span-2">
-                <p className="font-light text-gray-500">total</p>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            
-          </tbody>
-        </table>
+        <section className="w-full">
+          <div className="w-full grid grid-cols-12 border-b-[1px] border-gray-400 pb-4">
+            <div className="text-left col-span-1">
+              <p className="font-light text-gray-500">product</p>
+            </div>
+            <div className="text-left bg-red-200 col-span-6">
+              <p className="opacity-0">description</p>
+            </div>
+            <div className="text-left bg-blue-300 col-span-3">
+              <p className="font-light text-gray-500">quantity</p>
+            </div>
+            <div className="text-left  bg-green-200 col-span-2">
+              <p className="font-light text-gray-500">total</p>
+            </div>
+          </div>
+          <CartItems />
+        </section>
       </main>
     </div>
   );

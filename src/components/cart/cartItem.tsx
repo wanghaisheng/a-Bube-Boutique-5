7b39f6717164edfba43a32b46d4c39e0d1570cb0
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-13 15:13:52 +0100

    added delete item reducer action to delete item

--- src/components/cart/cartItem.tsx
+++ src/components/cart/cartItem.tsx
@@ -4,7 +4,7 @@ import { CartType } from "../../../typings";
 import { urlForImage } from "../../../sanity/lib/image";
 import Quantity from "../product/quantity";
 import { useState } from "react";
-import { setItemQuantity } from "@/lib/features/cartSlice";
+import { setItemQuantity, deleteItem } from "@/lib/features/cartSlice";
 import { useAppDispatch } from "@/lib/hook";
 
 export default function CartItem({ item }: { item: CartType }) {
@@ -52,6 +52,9 @@ export default function CartItem({ item }: { item: CartType }) {
             aria-hidden="true"
             focusable="false"
             className="w-5 h-5 cursor-pointer"
+            onClick={() =>
+              dispatch(deleteItem({ id: item.id, color: item.color }))
+            }
           >
             <path
               d="M14 3h-3.53a3.07 3.07 0 00-.6-1.65C9.44.82 8.8.5 8 .5s-1.44.32-1.87.85A3.06 3.06 0 005.53 3H2a.5.5 0 000 1h1.25v10c0 .28.22.5.5.5h8.5a.5.5 0 00.5-.5V4H14a.5.5 0 000-1zM6.91 1.98c.23-.29.58-.48 1.09-.48s.85.19 1.09.48c.2.24.3.6.36 1.02h-2.9c.05-.42.17-.78.36-1.02zm4.84 11.52h-7.5V4h7.5v9.5z"

dd4a47e40a2e37ae4f8f71925306c7b7040e0035
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-18 18:53:21 +0100

    added total section and checkout button

--- src/components/cart/cartItems.tsx
+++ src/components/cart/cartItems.tsx
@@ -3,12 +3,26 @@ import { useAppSelector } from "@/lib/hook";
 import CartItem from "./cartItem";
 
 export default function CartItems() {
-  const cart = useAppSelector((state) => state.cart.cart);
+  const { cart, total } = useAppSelector((state) => state.cart);
   return (
-    <div className="w-full flex flex-col gap-y-4 py-8">
-      {cart.map((item, idx) => (
-        <CartItem item={item} key={`${item.id}${item.color}`} />
-      ))}
-    </div>
+    <>
+      <div className="w-full flex flex-col gap-y-4 py-6 sm:py-8">
+        {cart.map((item, idx) => (
+          <CartItem item={item} key={`${item.id}${item.color}`} />
+        ))}
+      </div>
+      <div className="w-full flex flex-col items-end gap-y-4">
+        <div>
+          <p className="text-lg font-semibold">
+            <span className="mr-4 text-base font-normal">Estimated total</span>{" "}
+            <span className="font-normal">&#8358;</span>{" "}
+            {total.toLocaleString()}
+          </p>
+        </div>
+        <button className="w-72 py-2 bg-black text-white hover:scale-[1.02] transition-all duration-300">
+          Checkout
+        </button>
+      </div>
+    </>
   );
 }

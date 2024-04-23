7899936b83ae62316f330737aa25c19897b063b2
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-23 18:24:40 +0100

    destructued and typed the type  and pssed it to the getProduct function

--- src/pages/productDetailsPage.tsx
+++ src/pages/productDetailsPage.tsx
@@ -2,8 +2,14 @@ import { getProduct } from "@/queries/getProducts";
 import ProductImages from "@/components/product/productImages";
 import ProductInfo from "@/components/product/productInfo";
 
-export default async function ProductDetailsPage({ slug }: { slug: string }) {
-  const product = await getProduct(slug);
+export default async function ProductDetailsPage({
+  type,
+  slug,
+}: {
+  type: string;
+  slug: string;
+}) {
+  const product = await getProduct(type, slug);
   const item = product[0];
   return (
     <div className=" w-[90%] sm:w-[80%] mx-auto py-10 sm:py-16">

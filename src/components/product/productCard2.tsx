45363a9708c21131b66899386b3be2bd4e675b49
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-23 18:26:47 +0100

    changed between 2 images on hover of the product card

--- src/components/product/productCard2.tsx
+++ src/components/product/productCard2.tsx
@@ -3,23 +3,33 @@ import Image from "next/image";
 import { NewInItems } from "../../../typings";
 import { urlForImage } from "../../../sanity/lib/image";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function ProductCard2({ jewelry }: { jewelry: NewInItems }) {
   const [current, setCurrent] = useState(0);
   const { name, images, price, slug } = jewelry;
   return (
-    <div
-      className="group w-[15rem] sm:w-[25rem] lg:w-[20rem] space-y-4"
-      onMouseEnter={() => setCurrent(1)}
-      onMouseLeave={() => setCurrent(0)}
-    >
-      <div className="relative w-[15rem] h-[18rem] sm:w-[25rem] lg:w-[20rem] sm:h-[22rem]">
-        <Image src={urlForImage(images[current])} alt={name} className="" fill />
+    <Link href={`/products/jewelry/${slug}`}>
+      <div
+        className="group w-[15rem] sm:w-[25rem] lg:w-[20rem] space-y-4"
+        onMouseEnter={() => setCurrent(1)}
+        onMouseLeave={() => setCurrent(0)}
+      >
+        <div className="relative w-[15rem] h-[18rem] sm:w-[25rem] lg:w-[20rem] sm:h-[22rem]">
+          <Image
+            src={urlForImage(images[current])}
+            alt={name}
+            className=""
+            fill
+          />
+        </div>
+        <div className="space-y-1">
+          <h4 className="text-sm group-hover:underline group-hover:underline-offset-2">
+            BUBE {name}
+          </h4>
+          <p className="text-lg">&#8358; {price.toLocaleString()}</p>
+        </div>
       </div>
-      <div className="space-y-1">
-        <h4 className="text-sm group-hover:underline group-hover:underline-offset-2">BUBE {name}</h4>
-        <p className="text-lg">&#8358; {price.toLocaleString()}</p>
-      </div>
-    </div>
+    </Link>
   );
 }

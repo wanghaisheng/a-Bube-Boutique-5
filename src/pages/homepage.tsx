6ab275a941ea0c5d8067c1e81698739b99f0c86e
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-23 09:58:15 +0100

    added jewelry section component to homepage

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero/hero";
+import Jewelry from "@/components/home/jewelry";
 import NewIn from "@/components/home/newIn";
 
 export default function HomePage() {
@@ -6,6 +7,7 @@ export default function HomePage() {
     <>
       <Hero />
       <NewIn />
+      <Jewelry />
     </>
   );
 }

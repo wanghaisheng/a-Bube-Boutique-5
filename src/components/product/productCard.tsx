a6923588d4dd314d31245705721142fde45403a8
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-23 18:24:01 +0100

    edited the route to add the section, so i could use the slugs to get the items

--- src/components/product/productCard.tsx
+++ src/components/product/productCard.tsx
@@ -25,7 +25,7 @@ export default function ProductCard({
         <p className="text-sm text-[#737070]">{cardText}</p>
 
         <Link
-          href={`/products/${slug}`}
+          href={`/products/newIn/${slug}`}
           className="group w-full flex items-center gap-2 justify-center text-center"
         >
           <p className="group-hover:underline">Buy Now</p>

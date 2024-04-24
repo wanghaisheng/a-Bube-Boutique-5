4cd57f42efd3b097e5b7c24513e3174e78f179f6
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-24 17:14:23 +0100

    added the motto and collection sections components

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,5 +1,7 @@
 import Hero from "@/components/hero/hero";
+import Collections from "@/components/home/collections";
 import Jewelry from "@/components/home/jewelry";
+import Motto from "@/components/home/motto";
 import NewIn from "@/components/home/newIn";
 
 export default function HomePage() {
@@ -8,6 +10,8 @@ export default function HomePage() {
       <Hero />
       <NewIn />
       <Jewelry />
+      <Motto />
+      <Collections />
     </>
   );
 }

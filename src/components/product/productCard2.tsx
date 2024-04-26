dc21715415368937a6e3424b395e3a2b70b2884b
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-26 12:11:21 +0100

    destructured and typed the widthStyle and heightstyle props for styling changed div to a next link and removed outer link tag also added the appropiate routing with the slug

--- src/components/product/productCard2.tsx
+++ src/components/product/productCard2.tsx
@@ -5,30 +5,37 @@ import { urlForImage } from "../../../sanity/lib/image";
 import { useState } from "react";
 import Link from "next/link";
 
-export default function ProductCard2({ jewelry }: { jewelry: NewInItems }) {
+export default function ProductCard2({
+  jewelry,
+  widthStyle,
+  heightStyle,
+}: {
+  jewelry: NewInItems;
+  widthStyle: string;
+  heightStyle: string;
+}) {
   const [current, setCurrent] = useState(0);
   const { name, images, price, slug } = jewelry;
   return (
-    <Link href={`/products/jewelry/${slug}`}>
-      <div
-        className="group w-[15rem] sm:w-[25rem] lg:w-[20rem] space-y-4"
-        onMouseEnter={() => setCurrent(1)}
-        onMouseLeave={() => setCurrent(0)}
-      >
-        <div className="relative w-[15rem] h-[18rem] sm:w-[25rem] lg:w-[20rem] sm:h-[22rem]">
-          <Image
-            src={urlForImage(images[current])}
-            alt={name}
-            className=""
-            fill
-          />
-        </div>
-        <div className="space-y-1">
-          <h4 className="text-sm group-hover:underline group-hover:underline-offset-2">
-            BUBE {name}
-          </h4>
-          <p className="text-lg">&#8358; {price.toLocaleString()}</p>
-        </div>
+    <Link
+      href={`/products/jewelry/${slug}`}
+      className={`group ${widthStyle} space-y-4`}
+      onMouseEnter={() => setCurrent(1)}
+      onMouseLeave={() => setCurrent(0)}
+    >
+      <div className={`relative ${widthStyle} ${heightStyle}`}>
+        <Image
+          src={urlForImage(images[current])}
+          alt={name}
+          className=""
+          fill
+        />
+      </div>
+      <div className="space-y-1">
+        <h4 className="text-sm group-hover:underline group-hover:underline-offset-2">
+          BUBE {name}
+        </h4>
+        <p className="text-lg">&#8358; {price.toLocaleString()}</p>
       </div>
     </Link>
   );

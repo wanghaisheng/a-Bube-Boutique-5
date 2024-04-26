84a70332ae5b012fd2dc467f06f0b383869c312f
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-26 08:06:40 +0100

    changed export name

--- src/app/(client)/products/[[...slug]]/page.tsx
+++ src/app/(client)/products/[[...slug]]/page.tsx
@@ -1,13 +1,13 @@
 import ProductDetailsPage from "@/pages/productDetailsPage";
 
-export default async function ProductDetail({
+export default async function ProductDetails({
   params,
 }: {
   params: { slug: string[] };
 }) {
   return (
     <div className="">
-      <ProductDetailsPage  type={params.slug[0]} slug={params.slug[1]} />
+      <ProductDetailsPage type={params.slug[0]} slug={params.slug[1]} />
     </div>
   );
 }

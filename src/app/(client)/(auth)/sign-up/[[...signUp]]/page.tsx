e37fcf8e249f361659dc44d4945d028e3ee04540
immanuellking <emmaebubeegbosi@gmail.com>
2024-04-30 17:32:45 +0100

    sign up page for user authentication with the clerk signUp component mounted in it

---
+++ src/app/(client)/(auth)/sign-up/[[...signUp]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <div className="w-full flex justify-center py-12">
+    <SignUp signInUrl="/sign-in" />
+  </div>;
+}

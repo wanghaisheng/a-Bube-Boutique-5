0a9de07e154fb46abe6ab288e07a98ba714ce81e
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-03 23:29:05 +0100

    moved the code in then cart page file to  a seperate componenent

--- src/app/(client)/cart/page.tsx
+++ src/app/(client)/cart/page.tsx
@@ -1,31 +1,7 @@
-import CartItems from "@/components/cart/cartItems";
+import CartPage from "@/pages/cartPage";
 
-export default function CartPage() {
+export default function Page() {
   return (
-    <div className="">
-      <main className="w-[90%] lg:w-[70%] mx-auto py-12">
-        <div className="flex justify-between items-center mb-8">
-          <h1 className="text-3xl sm:text-4xl lg:text-5xl">Your Cart</h1>
-          <p className="underline">Continue Shopping</p>
-        </div>
-        <section className="w-full">
-          <div className="w-full grid grid-cols-12 border-b-[2px] border-gray-200 pb-4">
-            <div className="text-left col-span-3 sm:col-span-2 lg:col-span-1 uppercase">
-              <p className="font-light text-gray-500 text-sm">product</p>
-            </div>
-            <div className="text-left col-span-7 sm:col-span-5 lg:col-span-6 uppercase">
-              <p className="hidden text-gray-500 text-sm">description</p>
-            </div>
-            <div className="text-center hidden sm:flex sm:col-span-3 uppercase">
-              <p className="font-light text-gray-500 text-sm">quantity</p>
-            </div>
-            <div className="text-right col-span-2 uppercase">
-              <p className="font-light text-gray-500 text-sm">total</p>
-            </div>
-          </div>
-          <CartItems />
-        </section>
-      </main>
-    </div>
+    <CartPage />
   );
 }

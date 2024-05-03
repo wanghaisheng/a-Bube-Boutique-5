c9b8f590b26ccfe552d960bf7a3529d5aa1a9f5a
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-03 23:24:10 +0100

    moved sign in code and clerk sign in component to a cleint component so localstorage could work

---
+++ src/app/(client)/(auth)/sign-in/signInPage.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  const path = localStorage.getItem("backPage");
+  return (
+    <div className="w-full flex justify-center py-12">
+      <SignIn
+        forceRedirectUrl={path}
+        signUpUrl="/sign-up"
+      />
+      ;
+    </div>
+  );
+}

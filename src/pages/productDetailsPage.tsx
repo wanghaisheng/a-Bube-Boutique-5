249ba52bd78f742237688468fcdf9ea7719af051
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-05 00:41:48 +0100

    added you may also like section component to product details page

--- src/pages/productDetailsPage.tsx
+++ src/pages/productDetailsPage.tsx
@@ -1,6 +1,7 @@
 import { getProduct } from "@/queries/getProducts";
 import ProductImages from "@/components/product/productImages";
 import ProductInfo from "@/components/product/productInfo";
+import YouMayAlsoLike from "@/components/product/alsoLike";
 
 export default async function ProductDetailsPage({
   type,
@@ -12,11 +13,12 @@ export default async function ProductDetailsPage({
   const product = await getProduct(type, slug);
   const item = product[0];
   return (
-    <div className=" w-[90%] sm:w-[80%] mx-auto py-10 sm:py-16">
+    <div className=" w-[90%] sm:w-[80%] mx-auto py-10 sm:py-16 space-y-20">
       <div className="flex flex-col lg:flex-row gap-8">
         <ProductImages item={item} />
         <ProductInfo item={item} />
       </div>
+      <YouMayAlsoLike type={type} slug={slug} />
     </div>
   );
 }

444137e24ffc3f1d3ca1aa56b292304434bae32a
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-04 20:50:06 +0100

    made changes to event listeners, show drop down on mouse over

--- src/components/header/navheader.tsx
+++ src/components/header/navheader.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { IoIosArrowDown } from "react-icons/io";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function NavHeader() {
   const [open, setOpen] = useState(false);
@@ -11,32 +12,95 @@ export default function NavHeader() {
           <div className="relative">
             <div
               className="flex items-center gap-1 cursor-pointer hover:underline"
-              onClick={() => setOpen(!open)}
+              onMouseOver={() => setOpen(true)}
             >
-              <span>Products</span> <IoIosArrowDown size={15} className={`transition-all duration-300 ${open ? "rotate-180": ""}`} />
+              <span>Products</span>{" "}
+              <IoIosArrowDown
+                size={15}
+                className={`transition-all duration-300 ${
+                  open ? "rotate-180" : ""
+                }`}
+              />
             </div>
             <ul
               className={`w-48 absolute top-8 py-4 gap-y-3 border-[1px] bg-white border-[#c8c6c6] ${
                 open ? "flex flex-col z-50" : "hidden"
               }`}
+              onMouseEnter={() => setOpen(true)}
+              onMouseLeave={() => setOpen(false)}
             >
-              <li className="px-8 text-sm">Stargirl</li>
-              <li className="px-8 text-sm">Eternal</li>
-              <li className="px-8 text-sm">Wellness</li>
-              <li className="px-8 text-sm">Jewelry</li>
-              <li className="px-8 text-sm">Clothing</li>
-              <li className="px-8 text-sm">Accessories</li>
-              <li className="px-8 text-sm">Cover</li>
-              <li className="px-8 text-sm">Costumi</li>
-              <li className="px-8 text-sm">#hotmoresummer</li>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Stargirl
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Eternal
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Wellness
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Jewelry
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Clothing
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Accessories
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Cover
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                Costume
+              </Link>
+              <Link
+                href="/shop"
+                className="px-8 text-sm text-gray-500 hover:text-black hover:underline"
+              >
+                #hotmoresummer
+              </Link>
             </ul>
           </div>
         </li>
-        <li>Last Call</li>
-        <li>Social Responsibility</li>
-        <li>Concept</li>
-        <li>FAQ</li>
-        <li>How to find your ring size</li>
+        <Link href="/" className="cursor-pointer">
+          Last Call
+        </Link>
+        <Link href="/" className="cursor-pointer">
+          Social Responsibility
+        </Link>
+        <Link href="/" className="cursor-pointer">
+          Concept
+        </Link>
+        <Link href="/" className="cursor-pointer">
+          FAQ
+        </Link>
+        <Link href="/" className="cursor-pointer">
+          How to find your ring size
+        </Link>
       </ul>
     </div>
   );

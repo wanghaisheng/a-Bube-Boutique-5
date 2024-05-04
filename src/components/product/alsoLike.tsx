5d8a5b52d851c35dfd31434a96cb0457d9dfa86f
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-05 00:41:25 +0100

    you may also like section create, done and responisve

---
+++ src/components/product/alsoLike.tsx
@@ -0,0 +1,35 @@
+import { getNewInProducts } from "@/queries/getProducts";
+import ProductCard2 from "./productCard2";
+
+export default async function YouMayAlsoLike({
+  type,
+  slug,
+}: {
+  type: string;
+  slug: string;
+}) {
+  const products = await getNewInProducts(type);
+  return (
+    <div className="w-full space-y-8">
+      <h1 className="text-2xl">You may also like</h1>
+      <div className="flex snap-x snap-mandatory sm:grid sm:grid-cols-12 overflow-auto hide-x-scrollbar">
+        <div className="flex gap-x-4 sm:gap-x-10">
+          {products
+            .filter((item) => item.slug !== slug)
+            .map((item) => {
+              return (
+                <div className="snap-start snap-always">
+                <ProductCard2
+                  jewelry={item}
+                  key={item.id}
+                  widthStyle="w-[15rem] sm:w-[25rem] lg:w-[20rem] "
+                  heightStyle="h-[18rem] sm:h-[22rem]"
+                />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}

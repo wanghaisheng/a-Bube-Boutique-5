2835f57239cece2e2fc218a5e9a0ab5521ecfd43
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-04 08:44:42 +0100

    corrected property name

--- src/components/shop/filter.tsx
+++ src/components/shop/filter.tsx
@@ -15,8 +15,8 @@ export default function Filter({amount} : {amount: number}) {
               viewBox="0 0 10 6"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z"
                 fill="currentColor"
               ></path>
@@ -31,8 +31,8 @@ export default function Filter({amount} : {amount: number}) {
               viewBox="0 0 10 6"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z"
                 fill="currentColor"
               ></path>

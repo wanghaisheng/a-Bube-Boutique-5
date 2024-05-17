66b7f1a01a3dfd603997db923ed07b787c85b1b4
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-17 22:44:22 +0100

    changed text

--- src/components/home/emptySection.tsx
+++ src/components/home/emptySection.tsx
@@ -1,7 +1,7 @@
 export default function EmptySection() {
   return (
     <div className="w-full h-[40vh] flex items-center justify-center">
-      <h4 className="italic text-2xl">Couldn't retrieve New In Items 😔</h4>
+      <h4 className="italic text-2xl">Could not retrieve New In Items 😔</h4>
     </div>
   );
 }

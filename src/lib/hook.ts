08089a23004135996248602b631370810ae5988c
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-18 00:29:49 +0100

    removed the type when importng

--- src/lib/hook.ts
+++ src/lib/hook.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector, useStore } from 'react-redux';
-import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch, AppStore } from './store';
+import { TypedUseSelectorHook } from 'react-redux';
+import { RootState, AppDispatch, AppStore } from './store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

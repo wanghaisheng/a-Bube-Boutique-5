7ad786b17a73b48ee5929bb8863f82a4d3d0b380
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-18 00:44:44 +0100

    adjusted width and spacing

--- src/components/product/productImages.tsx
+++ src/components/product/productImages.tsx
@@ -7,12 +7,14 @@ import { useState } from "react";
 export default function ProductImages({ item }: { item: NewInItems }) {
   const [current, setCurrent] = useState(0);
   return (
-    <div className="flex flex-col-reverse sm:flex-row gap-4 w-full lg:w-[50%]">
+    <div className="flex flex-col-reverse sm:flex-row gap-8 w-full lg:w-[50%]">
       <div className="flex flex-row sm:flex-col gap-2 sm:gap-4">
-        {item.images?.map((image, index) => {
+        {item?.images?.map((image, index) => {
           return (
             <div
-              className={`relative w-[8rem] h-[6rem] sm:h-[8rem] border-[0.5px] border-gray-200 cursor-pointer ${current === index ? "ring-black ring-[1.5px]" : "ring-0"}`}
+              className={`relative w-[7rem] h-[6rem] sm:h-[8rem] border-[0.5px] border-gray-200 cursor-pointer ${
+                current === index ? "ring-black ring-[1.5px]" : "ring-0"
+              }`}
               key={index}
               onClick={() => setCurrent(index)}
             >
@@ -28,13 +30,15 @@ export default function ProductImages({ item }: { item: NewInItems }) {
         })}
       </div>
       <div className="relative w-full h-[28rem] sm:h-[38rem] lg:h-[35rem] border-[0.5px] border-gray-200">
-        <Image
-          src={urlForImage(item.images[current])}
-          alt={item.name}
-          className=""
-          loading="lazy"
-          fill
-        />
+        {item?.images && (
+          <Image
+            src={urlForImage(item?.images[current])}
+            alt={item.name}
+            className=""
+            loading="lazy"
+            fill
+          />
+        )}
       </div>
     </div>
   );

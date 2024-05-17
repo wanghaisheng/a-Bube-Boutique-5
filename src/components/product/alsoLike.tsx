f05a88ebddbc1fabecf736544f721ec6f8aaa3d4
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-18 00:31:45 +0100

    added key to the outer div

--- src/components/product/alsoLike.tsx
+++ src/components/product/alsoLike.tsx
@@ -1,5 +1,6 @@
 import { getNewInProducts } from "@/queries/getProducts";
 import ProductCard2 from "./productCard2";
+import { NewInItems } from "../../../typings";
 
 export default async function YouMayAlsoLike({
   type,
@@ -18,16 +19,15 @@ export default async function YouMayAlsoLike({
             .filter((item) => item.slug !== slug)
             .map((item) => {
               return (
-                <div className="snap-start snap-always">
+              <div className="snap-start snap-always" key={item.id}>
                 <ProductCard2
                   jewelry={item}
-                  key={item.id}
                   widthStyle="w-[15rem] sm:w-[25rem] lg:w-[20rem] "
                   heightStyle="h-[18rem] sm:h-[22rem]"
                 />
-                </div>
-              );
-            })}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>

5165ee783f6b0b4081a6d3f04ad589a0f7573abc
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-18 13:10:25 +0100

    resolved overflow issue cusing the body to have a scroll x

--- src/components/header/subheader.tsx
+++ src/components/header/subheader.tsx
@@ -1,6 +1,6 @@
 export default function SubHeader() {
   return (
-    <div className="w-full flex bg-black flex-nowrap">
+    <div className="w-full flex bg-black flex-nowrap overflow-hidden">
       <span className="flex items-center justify-center text-white text-xs sm:text-[13px] text-nowrap font-custom py-2 font-semibold tracking-wider md:justify-start animate-infinite-scroll">
         $10 EXPRESS COURIER. FREE SHIPPING FOR ORDERS $200+. SUPRISE GIFTS WITH
         PURCHASES $250+.

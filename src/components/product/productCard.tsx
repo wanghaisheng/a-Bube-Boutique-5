f22254b733d6724fbddceaf14451154cc5cc2467
immanuellking <emmaebubeegbosi@gmail.com>
2024-05-25 15:54:44 +0100

    reduce product card width on mobile

--- src/components/product/productCard.tsx
+++ src/components/product/productCard.tsx
@@ -8,16 +8,12 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 export const revalidate = 60;
 
-export default function ProductCard({
-  newInItem,
-}: {
-  newInItem: NewInItems;
-}) {
+export default function ProductCard({ newInItem }: { newInItem: NewInItems }) {
   const { name, images, cardText, slug, _type } = newInItem;
 
   return (
-    <div className="w-[22rem] sm:w-full sm:col-span-6 lg:col-span-4 bg-gray-100 snap-center snap-always">
-      <div className="relative w-[25rem] sm:w-full h-[22rem] sm:h-[25rem] lg:h-[22rem]">
+    <div className="w-[21rem] sm:w-full sm:col-span-6 lg:col-span-4 bg-gray-100 snap-center snap-always">
+      <div className="relative w-[21rem] sm:w-full h-[22rem] sm:h-[25rem] lg:h-[22rem]">
         <Image src={urlForImage(images[0])} alt={name} className="" fill />
       </div>
       <div className="text-center py-4 px-4 space-y-2">
